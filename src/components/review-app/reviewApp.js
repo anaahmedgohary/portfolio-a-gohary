2b8ae423c5765e13271eb9377e9f46d871065bea
anaahmedgohary <gogoahmed13@gmail.com>
2022-08-22 15:08:26 +0200

    a onClick

--- src/components/review-app/reviewApp.js
+++ src/components/review-app/reviewApp.js
@@ -6,13 +6,11 @@ function ReviewApp()
 {
 
 
-    if (document.readyState === "loading") {
-      document.addEventListener("DOMContentLoaded", ready);
-    } else {
-      ready();
-    }
+    
+  ready();
+    
 
-    function ready() {
+    function ready(){
       let serviceName = $("#serviceName");
       let ratingOverall = $("#ratingOverall");
       let custServ = $("#custServ");
@@ -20,15 +18,6 @@ function ReviewApp()
       let qualityRate = $("#qualityRate");
       let cleanRate = $("#cleanRate");
       let paragraphRev = $("#paragraphRev");
-
-
-      serviceName.text($("#bNameInp").val());
-      ratingOverall.text($("#ovRateInp").val());
-      custServ.text($("#custServInp").val());
-      staffRate.text($("#staffInp").val());
-      qualityRate.text($("#qualityInp").val());
-      cleanRate.text($("#cleanInp").val());
-      paragraphRev.text($("#letterInp").val());
 
       $("#submit-rev-btn").click(() => {
         //alert("Are You Sure?")
@@ -54,82 +43,172 @@ function ReviewApp()
             $(this).val(5);;
           }
         });
-      }
-    }
+      };
+
+      serviceName.text($("#bNameInp").val());
+      ratingOverall.text($("#ovRateInp").val());
+      custServ.text($("#custServInp").val());
+      staffRate.text($("#staffInp").val());
+      qualityRate.text($("#qualityInp").val());
+      cleanRate.text($("#cleanInp").val());
+      paragraphRev.text($("#letterInp").val());
+
+    };
+    
     
 
 
   return (
-    <div>
-        <div>
-        <div>
-            <nav className="main-nav">
-                <span><a href="index.html">Home</a></span>
-                <span><a href="contact.html">Contact</a></span>
-                <span><a href="popular.html">Popular</a></span>
-                <span><a href="new.html">New</a></span>
-            </nav>
-        </div>
-        <div id="searchBarDiv">
-            <input id="searchBarInp" type="search" placeholder="Search me!" />
-            <div className="half-circle-left"></div>
-            <div className="half-circle-right"></div>
-        </div>
-    </div>
-    <section>
+    <>
+      <div>
+        <nav className="main-nav">
+          <span>
+            <a href="index.html">Home</a>
+          </span>
+          <span>
+            <a href="contact.html">Contact</a>
+          </span>
+          <span>
+            <a href="popular.html">Popular</a>
+          </span>
+          <span>
+            <a href="new.html">New</a>
+          </span>
+        </nav>
+      </div>
+      <div id="searchBarDiv">
+        <input id="searchBarInp" type="search" placeholder="Search me!" />
+        <div className="half-circle-left"></div>
+        <div className="half-circle-right"></div>
+      </div>
+
+      <section>
         <div className="review-div">
-            <h2>Add Your Review Now!</h2>
-            <div>
-                <span className="column-1">Name of Business</span>
-                <input id="bNameInp" className="column-2" type="text"  placeholder="Business/Service name" />
-            </div>
-            <div>
-                <span className="column-1">First Impression</span>
-                <input id="ovRateInp" className="column-2 rate-0-5" type="number" maxlength="1" max="5" minlength="1" min="0" step="0.5" />
-            </div>
-            <div>
-                <span className="column-1">Customer Service</span>
-                <input id="custServInp" className="column-2 rate-0-5" type="number" maxlength="1" max="5" minlength="1" min="0" step="0.5" />
-            </div>
-            <div>
-                <span className="column-1">Staff Treatment</span>
-                <input id="staffInp" className="column-2 rate-0-5" type="number" maxlength="1" max="5" minlength="1" min="0" step="0.5" />
-            </div>
-            <div>
-                <span className="column-1">Quality Of Product</span>
-                <input id="qualityInp" className="column-2 rate-0-5" type="number" maxlength="1" max="5" minlength="1" min="0" step="0.5" />
-            </div>
-            <div>
-                <span className="column-1">Cleanliness Standard</span>
-                <input id="cleanInp" className="column-2 rate-0-5" type="number" maxlength="1" max="5" minlength="1" min="0" step="0.5" />
-            </div>
-            <div>
-                <span className="column-1">Short letter</span>
-                <input id="letterInp" className="column-2" type="text" maxlength="200" />
-            </div>
-            <div><input id="submit-rev-btn" className="column-2" type="submit" value="Submit" /></div>
+          <h2>Add Your Review Now!</h2>
+          <div>
+            <span className="column-1">Name of Business</span>
+            <input
+              id="bNameInp"
+              className="column-2"
+              type="text"
+              placeholder="Business/Service name"
+            />
+          </div>
+          <div>
+            <span className="column-1">First Impression</span>
+            <input
+              id="ovRateInp"
+              className="column-2 rate-0-5"
+              type="number"
+              maxLength="1"
+              max="5"
+              minLength="1"
+              min="0"
+              step="0.5"
+            />
+          </div>
+          <div>
+            <span className="column-1">Customer Service</span>
+            <input
+              id="custServInp"
+              className="column-2 rate-0-5"
+              type="number"
+              maxLength="1"
+              max="5"
+              minLength="1"
+              min="0"
+              step="0.5"
+            />
+          </div>
+          <div>
+            <span className="column-1">Staff Treatment</span>
+            <input
+              id="staffInp"
+              className="column-2 rate-0-5"
+              type="number"
+              maxLength="1"
+              max="5"
+              minLength="1"
+              min="0"
+              step="0.5"
+            />
+          </div>
+          <div>
+            <span className="column-1">Quality Of Product</span>
+            <input
+              id="qualityInp"
+              className="column-2 rate-0-5"
+              type="number"
+              maxLength="1"
+              max="5"
+              minLength="1"
+              min="0"
+              step="0.5"
+            />
+          </div>
+          <div>
+            <span className="column-1">Cleanliness Standard</span>
+            <input
+              id="cleanInp"
+              className="column-2 rate-0-5"
+              type="number"
+              maxLength="1"
+              max="5"
+              minLength="1"
+              min="0"
+              step="0.5"
+            />
+          </div>
+          <div>
+            <span className="column-1">Short letter</span>
+            <input
+              id="letterInp"
+              className="column-2"
+              type="text"
+              maxLength="200"
+            />
+          </div>
+          <div>
+            <input
+              onClick={ready}
+              id="submit-rev-btn"
+              className="column-2"
+              type="submit"
+              value="Submit"
+            />
+          </div>
         </div>
-    </section>
-    <section>
+      </section>
+      <section>
         <div className="new-reviews hidden">
-            <h4 id="serviceName">aaaaa</h4>
-            <label className="new-label" for="">First Impression</label>
-            <span className="new-rates" id="ratingOverall">aaaaaa</span>
-            <label className="new-label" for="">Customer Service</label>
-            <span className="new-rates" id="custServ">aaaaaaa</span>
-            <label className="new-label" for="">Staff</label>
-            <span className="new-rates" id="staffRate"> aaaaa</span>
-            <label className="new-label" for="">Quality</label>
-            <span className="new-rates" id="qualityRate">aaaaaaaa</span>
-            <label className="new-label" for="">Cleanliness</label>
-            <span className="new-rates" id="cleanRate">aaaaaaa</span>
-            <label className="letter-label" for="">Short letter</label>
-            <p id="paragraphRev">aaaaaaaaa</p>
-            
+          <h4 id="serviceName">aaaaa</h4>
+          <label className="new-label">First Impression</label>
+          <span className="new-rates" id="ratingOverall">
+            aaaaaa
+          </span>
+          <label className="new-label">Customer Service</label>
+          <span className="new-rates" id="custServ">
+            aaaaaaa
+          </span>
+          <label className="new-label">Staff</label>
+          <span className="new-rates" id="staffRate">
+            {" "}
+            aaaaa
+          </span>
+          <label className="new-label">Quality</label>
+          <span className="new-rates" id="qualityRate">
+            aaaaaaaa
+          </span>
+          <label className="new-label">Cleanliness</label>
+          <span className="new-rates" id="cleanRate">
+            aaaaaaa
+          </span>
+          <label className="letter-label">Short letter</label>
+          <p id="paragraphRev">aaaaaaaaa</p>
         </div>
-    </section>
-    </div>
-  )
+      </section>
+    </>
+  );
 }
 
 export default ReviewApp;
