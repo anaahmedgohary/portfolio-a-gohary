31b4d7c81b28971838f3cb4d965e0b25201d0626
anaahmedgohary <gogoahmed13@gmail.com>
2022-08-29 20:13:09 +0200

    add image & fix header

--- src/components/landing/header.js
+++ src/components/landing/header.js
@@ -12,12 +12,12 @@ function Header()
             <img class="profile-img" src="./images/000.jpg" alt="" />
           </div>
 
-          <div class="und-pic container">
+          <div class="und-pic">
             <div class="hi-myname">
               <img src="./images/Hi-my-name-is-Ahmed-8-10-20225.png" alt="" />
             </div>
-            <div></div>
-            <div class="contact-container container">
+            
+            <div class="contact-container">
               <h3>Contact Information</h3>
               <p class="row">Email: gogoahmed13@gmail.com</p>
               <p class="row">phone: +201155267277</p>
