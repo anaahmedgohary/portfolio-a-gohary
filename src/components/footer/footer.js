bed6a0827c7eaa80e208c546538674a8eb5aeb9d
anaahmedgohary <gogoahmed13@gmail.com>
2022-10-13 12:47:58 +0200

    update131022B

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,21 +5,21 @@ const Footer = () => {
     return (
         <div className='portfolio-footer'>
             <div className='my-contacts container'>
-                <h5>Contact</h5>
-                <div>
+                <h5>Contact Me</h5>
+                <div className='footer-CE-div'>
                     <p className="footer-info-row">
-                        <strong>Email:   </strong>
+                        <label>Email</label>
                         <a className='btn-success btn' href="mailto:gogoahmed13@gmail.com" target="_blank" rel="noopener noreferrer">
                             gogoahmed13@gmail.com
                         </a>
                     </p>
                     <p className="footer-info-row">
-                        <strong>Phone: </strong>
+                        <label>Phone</label>
                         <a className='btn-success btn' href="tel:+201090371068">+201090371068</a>
                     </p>
                     
                     <p className="footer-info-row">
-                        <strong>Linkedin:</strong>
+                        <label>Linkedin</label>
                         <a className='btn-success btn' href="https://www.linkedin.com/in/ahmed-gohary-a67963192/" target="_blank" rel="noopener noreferrer">
                             Linkedin profile link
                         </a>
--- src/components/landing/myprojects.js
+++ src/components/landing/myprojects.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import "./style/bodybstyle.css"
+import "./style/myprojects/bodybstyle.css"
 
 export default function MyProjects()
 {
