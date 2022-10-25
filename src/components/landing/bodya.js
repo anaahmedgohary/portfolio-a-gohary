b395660df556b2cd09ef7458ceea11cbb62179f7
anaahmedgohary <gogoahmed13@gmail.com>
2022-10-25 05:49:53 +0200

    251022

--- src/components/landing/bodya.js
+++ src/components/landing/bodya.js
@@ -1,10 +1,13 @@
 import React from 'react';
 // import { useState } from 'react';
 import "./style/style.css";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function Bodya()
 {
 
+  AOS.init(500);
   //const [oshow, setOshow] = useState("ohide");
 
   // const observer = new IntersectionObserver((entries) =>
@@ -23,7 +26,8 @@ function Bodya()
 
   // const hiddenEls = document.querySelectorAll('.oHide');
   // hiddenEls.forEach((el)=> observer.observe(el));
-
+  let fadeleft = "fade-left";
+  let faderight = 'fade-right';
 
 
   return (
@@ -35,25 +39,36 @@ function Bodya()
             className="card-img-top img-sskills"
             src="./images/Skills04.png"
             alt="My Skills"
+            data-aos='fade-down'
           />
-          <div className="skills-div">
-            <p className='my-skill'>HTML</p>
-            <p className='my-skill'>CSS</p>
-            <p className='my-skill'>JavaScript</p>
-            <p className='my-skill'>jQuery</p>
-            <p className='my-skill'>Git</p>
-            <p className='my-skill'>Github</p>
-            <p className='my-skill'>SASS.scss</p>
-            <p className='my-skill'>BootStrap</p>
-            <p className='my-skill'>REACT.js</p>
-            <p className='my-skill'>Adobe Photoshop</p>
-            <p className='my-skill'>Adobe Illustrator</p>
-            <p className='my-skill'>Adobe XD</p>
-            <p className='my-skill'>Adobe Premiere</p>
-            <p className='my-skill'>GIMP</p>
-            <p className='my-skill'>Microsoft Office</p>
-            <p className='my-skill'>MySQL</p>
-            <p className='my-skill'>PHP</p>
+          <div className="skills-div"
+            data-aos='fade-up'
+            data-aos-duration="500">
+            <p
+              data-aos={faderight} className='my-skill'>HTML</p>
+            <p
+              data-aos={faderight} className='my-skill'>CSS</p>
+            <p data-aos={faderight} className='my-skill'>JavaScript</p>
+            <p
+              data-aos={faderight} className='my-skill'>jQuery</p>
+            <p
+              data-aos={faderight} className='my-skill'>Git</p>
+            <p data-aos={fadeleft} className='my-skill'>Github</p>
+            <p data-aos={fadeleft} className='my-skill'>SASS.scss</p>
+            <p data-aos={fadeleft} className='my-skill'>BootStrap</p>
+            <p data-aos={fadeleft} className='my-skill'>REACT.js</p>
+            <p
+              data-aos={faderight} className='my-skill'>Adobe Photoshop</p>
+            <p
+              data-aos={faderight} className='my-skill'>Adobe Illustrator</p>
+            <p
+              data-aos={faderight} className='my-skill'>Adobe XD</p>
+            <p data-aos={fadeleft} className='my-skill'>Adobe Premiere</p>
+            <p data-aos={fadeleft} className='my-skill'>GIMP</p>
+            <p data-aos={fadeleft} className='my-skill'>Microsoft Office</p>
+            <p
+              data-aos={faderight} className='my-skill'>MySQL</p>
+            <p data-aos={fadeleft} className='my-skill'>PHP</p>
           </div>
         </div>
 
@@ -62,8 +77,10 @@ function Bodya()
             className="card-img-top about-img"
             src="./images/facerj.jpg"
             alt="profile"
+            data-aos="fade-left"
+            data-aos-duration="1000"
           />
-          <div className="card-body">
+          <div className="card-body" data-aos="fade-right" data-aos-duration="1000">
             <h3 className="card-title">About Me</h3>
             <div className="about-content">
               <p>
--- src/components/landing/myprojects.js
+++ src/components/landing/myprojects.js
@@ -1,104 +1,114 @@
 import React from 'react'
 
 import "./style/myprojects/bodybstyle.css"
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function MyProjects()
 {
-    return (
-      <div className="proj-body">
-        <h1 className="proj-title">My Projects</h1>
+  AOS.init();
 
-        <div className="proj-cards">
-          <div className="card proj-card pc01">
-            <img
-              className="card-img-top card-image"
-              src="./images/menusvercel.jpg"
-              alt="website screenpshot"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Menus web app with (REACT)</h5>
-              <p className="card-text">
-                Project of web app for Menus made with REACT.JS framework ,
-                Bootstrap & SASS.
-              </p>
-              <a
-                href="https://menusapp-ahmedgohary.vercel.app"
-                className="btn btn-primary"
-              >
-                Link to project
-              </a>
-            </div>
+  return (
+    <div className="proj-body">
+      <h1 className="proj-title">My Projects</h1>
+
+      <div className="proj-cards">
+
+        <div className="card proj-card pc01" data-aos='fade-up' data-aos-duration="500" data-aos-easing="ease-out">
+          <img
+            className="card-img-top card-image"
+            src="./images/reactRestaurantThump.png"
+            alt="website screenpshot"
+          />
+          <div className="card-body">
+            <h5 className="card-title">Resturant web app with Cart (REACT)</h5>
+            <p className="card-text">
+              Project of website for resturant web app with dynamic Cart made
+              with REACT JS framework.
+            </p>
+            <a
+              href="https://react-restaurant-git-master-ahmedgohary.vercel.app/"
+              className="btn btn-primary"
+            >
+              Link to project
+            </a>
           </div>
+        </div>
 
-          <div className="card proj-card pc02">
-            <img
-              className="card-img-top card-image"
-              src="./images/shahd01.png"
-              alt="website screenpshot"
-            />
-            <div className="card-body">
-              <h5 className="card-title">
-                Web app for online shopping. [MySQL DB. PHP. local storage.
-                Paypal.]
-              </h5>
-              <p className="card-text">
-                Project of website for online shopping. Connected to MySQL
-                Database. Stores Cart data in local storage. Added Paypal
-                Payment method. Made with [PHP, HTML, JavaScript, CSS, MySQL,
-                jQuery].
-              </p>
-              <a
-                href="http://shahdresturant.epizy.com/"
-                className="btn btn-primary"
-              >
-                Link to project
-              </a>
-            </div>
+        <div className="card proj-card pc02" data-aos='fade-up' data-aos-duration="500" data-aos-easing="ease-out">
+          <img
+            className="card-img-top card-image"
+            src="./images/shahd01.png"
+            alt="website screenpshot"
+          />
+          <div className="card-body">
+            <h5 className="card-title">
+              Web app for online shopping. [MySQL DB. PHP. local storage.
+              Paypal.]
+            </h5>
+            <p className="card-text">
+              Project of website for online shopping. Connected to MySQL
+              Database. Stores Cart data in local storage. Added Paypal
+              Payment method. Made with [PHP, HTML, JavaScript, CSS, MySQL,
+              jQuery].
+            </p>
+            <a
+              href="http://shahdresturant.epizy.com/"
+              className="btn btn-primary"
+            >
+              Link to project
+            </a>
           </div>
+        </div>
 
-          <div className="card proj-card pc03">
-            <img
-              className="card-img-top card-image"
-              src="./images/reactRestaurantThump.png"
-              alt="website screenpshot"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Resturant web app with Cart (REACT)</h5>
-              <p className="card-text">
-                Project of website for resturant web app with dynamic Cart made
-                with REACT JS framework.
-              </p>
-              <a
-                href="https://react-restaurant-git-master-ahmedgohary.vercel.app/"
-                className="btn btn-primary"
-              >
-                Link to project
-              </a>
-            </div>
+        <div className="card proj-card pc03" data-aos='fade-up' data-aos-delay="100" data-aos-easing="ease-out">
+          <img
+            className="card-img-top card-image"
+            src="./images/menusvercel.jpg"
+            alt="website screenpshot"
+          />
+          <div className="card-body">
+            <h5 className="card-title">Menus web app with (REACT)</h5>
+            <p className="card-text">
+              Project of web app for Menus made with REACT.JS framework ,
+              Bootstrap & SASS.
+            </p>
+            <a
+              href="https://menusapp-ahmedgohary.vercel.app"
+              className="btn btn-primary"
+            >
+              Link to project
+            </a>
           </div>
+        </div>
 
-          <div className="card proj-card pc04">
-            <img
-              className="card-img-top card-image"
-              src="./images/reviewsThump.png"
-              alt="website screenpshot"
-            />
-            <div className="card-body">
-              <h5 className="card-title">
-                Customer reviews web app, connected to MySQL Database. And
-                Backend API.
-              </h5>
-              <p className="card-text">
-                Project of website for Customer reviews made with with [PHP,
-                HTML, SASS, JavaScript, CSS, MySQL, jQuery].
-              </p>
-              <a href="http://review-app.epizy.com/" className="btn btn-primary">
-                Link to project
-              </a>
-            </div>
+        
+
+        
+
+        <div className="card proj-card pc04" data-aos='fade-up' data-aos-duration="500" data-aos-easing="ease-out"
+>
+          <img
+            className="card-img-top card-image"
+            src="./images/reviewsThump.png"
+            alt="website screenpshot"
+          />
+          <div className="card-body">
+            <h5 className="card-title">
+              Customer reviews web app, connected to MySQL Database. And
+              Backend API.
+            </h5>
+            <p className="card-text">
+              Project of website for Customer reviews made with with [PHP,
+              HTML, SASS, JavaScript, CSS, MySQL, jQuery].
+            </p>
+            <a href="http://review-app.epizy.com/" className="btn btn-primary">
+              Link to project
+            </a>
           </div>
+        </div>
 
-          {/* <div className="card proj-card pc05">
+        {/* <div className="card proj-card pc05" data-aos='fade-up'>
             <img
               className="card-img-top card-image"
               src="./images/reviewSite.jpg"
@@ -117,7 +127,7 @@ export default function MyProjects()
           </div>
         </div>
         <div> */}
-          {/* <div>
+        {/* <div>
                     <h1>
                         Customer reviews web app with vanilla Javascript
                     </h1>
@@ -130,9 +140,9 @@ export default function MyProjects()
                     </h1>
                     <a href="https://react-shahd-git-main-marlojoman.vercel.app/" target="_blank" rel="noopener noreferrer">Link to REACT project</a>
                 </div> */}
-        </div>
       </div>
-    );
+    </div>
+  );
 }
 
 
